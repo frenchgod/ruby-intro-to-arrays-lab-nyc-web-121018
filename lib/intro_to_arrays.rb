80bff403662da59712be68e418fc00f11a5f8d61
github username <github email address>
2018-12-01 18:43:33 -0500

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,41 @@
+def instantiate_new_array
+array = []
+array
+end
+
+def array_with_two_elements
+  juice = ["apple", "orange"]
+  juice
+end
+
+ def first_element(array)
+juice = ["Welcome to New York", "apple", "cranberry", "tangerine"]
+   juice[0]
+ end
+
+ def third_element(array)
+   juice = ["orange", "apple", "Style", "tangerine"]
+   juice[2]
+ end
+
+ def last_element(array)
+   juice = ["Welcome to New York", "apple", "Style", "Out of The Woods"]
+   juice[-1]
+ end
+
+
+def first_element_with_array_methods(array)
+juice = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+juice.shift
+
+end
+
+def last_element_with_array_methods(array)
+  juice = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  juice.pop
+end
+
+def length_of_array (array)
+  juice = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  juice.length
+end
